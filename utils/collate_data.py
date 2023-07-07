231c28db6484a9a2f2ed2ccb0d525fa8743a7b25
shoryaconsul <shoryaconsul@gmail.com>
2023-07-07 18:40:37 -0500

    script to label and pickle data

--- utils/collate_data.py
+++ utils/collate_data.py
@@ -5,79 +5,56 @@ Script to create dataset from the raw data
 from datetime import datetime
 '''
 import os
-import pandas as pd
+import numpy as np
 import argparse
 from datetime import datetime
 from tqdm import tqdm
-from utils.filtering import msv
-from utils.specific_tools import auto_label
-from utils.plotting import plot_graph
+import bz2
+import pickle
+# from utils.plotting import plot_graph
 
 
 def main(args):
     # Set file paths
-    raw_data_path = 'data/Raw/'
-    labels_path = 'data/Labels/labels legend.csv'
-    training_data_path = 'data/Processed/Training/processed_data.csv'
+    raw_data_path = 'data/'
+    viral_read_files = ['hpv_reads']
+    nonviral_read_files = ['human_reads']
+    output_pkl = 'data/proc_data.pkl'
 
-    # Load labels data
-    labels_data = pd.read_csv(labels_path)
-
-    # Create a dictionary of raw data file paths
-    raw_data_files = {}
-    for filename in os.listdir(raw_data_path):
-        if filename.endswith('.csv'):
-            raw_data_files[filename.split('.')[0]] = os.path.join(
-                raw_data_path, filename)
-
-    # Create empty dataframes for training and random data
-    # Data frame has columns for sentence, label
-    training_data = pd.DataFrame(columns=['sentence', 'label'])
-
-    # Loop through each row in the labels data and extract corresponding raw data
-    for index, row in tqdm(labels_data.iterrows(), total=labels_data.shape[0]):
-        # Get the file name and label
-
-        filename = row['CSV'].split('.')[0]
-        label = 'data/Labels/' + row['Label csv file']
-        input_date_str = filename.split('_')[1]
-        input_date = datetime.strptime(input_date_str, '%Y-%m-%d')
-        data_tag = input_date.strftime('%b%d')+'_'+str(row['BPM'])+'BPM'
-        training_flag = True if row['Sequence'] == 'Training' else False
-        # Check if raw data file exists for the file name
-        if filename in raw_data_files:
-            # Load raw data
-            raw_data = msv(raw_data_files[filename], row['Time start'],
-                           row['Time end'], 3, data_tag, 'All')
-            # data_senteneces: (n_sentences, n_words, n_channels, data_word_len)
-            # label_sentences: (n_sentences, n_words)
-            data_sentences, label_sentences = auto_label(label, raw_data,
-                                                         row['BPM'], args.sampling_rate, args.data_word_len, training_flag)
-            # Plot the graph and save it
-            plot_graph(data_sentences, label_sentences,
-                       'data/Processed/'+filename+'.png')
-
-            # Append data_sentences and corresponding labels to training data
-            for s_idx, sentence in enumerate(data_sentences):
-                training_data.loc[len(training_data)] = [
-                    sentence, label_sentences[s_idx]]
-
-    # Save processed training data
-    training_folder = '/'.join(training_data_path.split('/')[:-1])
-    if not os.path.exists(training_folder):
-        os.makedirs(training_folder)
-    training_data.to_csv(training_data_path)
-    training_data.to_pickle(training_data_path.split('.')[0]+'.pkl')
-    print("Number of sentence in Training dataset: ", training_data.shape[0])
-
-    print("Collated complete!")
+    # Reading viral data files
+    vread_list = []
+    for vf in viral_read_files:
+        vfile = os.path.join(raw_data_path, vf) + '.txt'
+        vreads = np.loadtxt(vfile)
+        if vreads.shape[1] != args.length:  # Check that reads are of correct length
+            raise ValueError('Reads in %s are not %d bp.' %(vfile, args.length))
+        vread_list.append(vreads)
+    viral_reads = np.vstack(vread_list)
+    viral_labels = np.ones(viral_reads.shape[0], dtype=np.int8)
+    
+    # Reading non-viral data files
+    nvread_list = []
+    for nvf in nonviral_read_files:
+        nvfile = os.path.join(raw_data_path, nvf) + '.txt'
+        nvreads = np.loadtxt(nvfile)
+        if nvreads.shape[1] != args.length:  # Check that reads are of correct length
+            raise ValueError('Reads in %s are not %d bp.' %(nvfile, args.length))
+        nvread_list.append(nvreads)
+    nonviral_reads = np.vstack(nvread_list)
+    nonviral_labels = np.zeros(nonviral_reads.shape[0], dtype=np.int8)
+    
+    # Pickle data objects
+    data_dict = {'reads': np.vstack((viral_reads, nonviral_reads)),
+                'labels': np.concatenate((viral_labels, nonviral_labels))}
+    with bz2.BZ2File(output_pkl,'wb') as pfile:
+        pickle.dump(data_dict, pfile, protocol=4)
+    
+    print('Data processing completed')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--sampling_rate', type=int, default=250,
-                        help='Sampling rate of the data')
-    parser.add_argument('--data_word_len', type=int, default=25,
-                        help='Length of the data word')
+    parser.add_argument('-l', '--length', type=int, default=150,
+                    help="Length of reads")
     args = parser.parse_args()
     main(args)
