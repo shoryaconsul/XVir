2922c316a8d22c13358451efd0c2a41c22879196
shoryaconsul <shoryaconsul@gmail.com>
2024-05-06 07:30:25 -0500

    generalized scripts to improve readability

--- data/gen_aug_data.sh
+++ data/gen_aug_data.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-INPUT_FILE="reads/split/reads_250_train.fa"
-OUTLEN=250
-OUTPRE="reads/split/reads_"
+# Generate 10% augmented data. To vary the fraction to x%, change the value of -a to x/2%.
+
+# INPUT_FILE="reads/split/reads_250_train.fa"
+# OUTPRE="reads/split/reads_"
+# OUTLEN-150
+
+INPUT_FILE="mixture/split/train.fa"
+OUTPRE="mixture/split/train_"
+OUTLEN=150
+
+INPUT_PRE=${INPUT_FILE%.*}
 
 # Generate mutated reads
 python mutate_reads.py $INPUT_FILE $OUTPRE$OUTLEN"_aug_mut_5.fa" -l $OUTLEN -s 0.0375 -i 0.00625 -d 0.00625 --aug
@@ -15,8 +23,9 @@ python mutate_reads.py $INPUT_FILE $OUTPRE$OUTLEN"_aug_mut_20.fa" -l $OUTLEN -s 
 cat $OUTPRE$OUTLEN"_aug_mut_"*".fa" > $OUTPRE$OUTLEN"_aug_mut.fa"
 
 # Sample reads
-python sample_fasta.py $OUTPRE$OUTLEN"_aug_mut.fa" -o $OUTPRE$OUTLEN"_aug_samp_0.1.fa" -a 0.05  
-cat "reads/split/reads_"$OUTLEN"_train.fa" $OUTPRE$OUTLEN"_aug_samp_0.1.fa" >  "reads/split/reads_"$OUTLEN"_train_aug_0.1.fa"
+python sample_fasta.py $OUTPRE$OUTLEN"_aug_mut.fa" -o $OUTPRE$OUTLEN"_aug_samp_0.1.fa" -a 0.05  # 0.05 = 10% augmentation
+cat $INPUT_FILE $OUTPRE$OUTLEN"_aug_samp_0.1.fa" >  $INPUT_PRE"_aug_0.1.fa"
 
 # Pickle data for use by XVir
-python pickle_fasta.py -l $OUTLEN -f "reads/split/reads_"$OUTLEN"_train_aug_0.1.fa" -o reads/split/train_data_"$OUTLEN"bp_aug_0.1.pkl
+# echo "{$(dirname $INPUT_FILE)"/train_data_"$OUTLEN"bp_aug_0.1.pkl"}"
+python pickle_fasta.py -l $OUTLEN -f $INPUT_PRE"_aug_0.1.fa" -o $(dirname $INPUT_FILE)"/train_data_"$OUTLEN"bp_aug_0.1.pkl"
